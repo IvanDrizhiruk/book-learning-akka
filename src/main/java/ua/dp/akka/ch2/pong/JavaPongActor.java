1d2bf2eae18dc1b778f878e4f532f9ecd07564a4
IvanDrizhiruk <ivan.drizhiruk@gmail.com>
2017-10-11 09:31:49 +0300

    Chapter 2

---
+++ src/main/java/ua/dp/akka/ch2/pong/JavaPongActor.java
@@ -0,0 +1,18 @@
+package ua.dp.akka.ch2.pong;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Status;
+import akka.japi.pf.ReceiveBuilder;
+
+public class JavaPongActor extends AbstractActor {
+
+	@Override
+	public Receive createReceive() {
+		return ReceiveBuilder
+				.create()
+				.matchEquals("Ping", s -> sender().tell("Pong", ActorRef.noSender()))
+				.matchAny(x ->sender().tell(new Status.Failure(new Exception("unknown message")), self()))
+				.build();
+	}
+}
