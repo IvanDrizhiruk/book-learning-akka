7d45d2b73005b4d93f9c4259f9842db386e5c9d5
Ivan Drizhiruk <ivan.drizhiruk@gmail.com>
2017-10-21 11:41:03 +0300

    Home work has been updated

--- ch2/homework/src/main/java/ua/dp/akka/ReverserActor.java
+++ ch2/homework/src/main/java/ua/dp/akka/ReverserActor.java
@@ -14,16 +14,33 @@ public class ReverserActor extends AbstractActor {
 
         return ReceiveBuilder.create()
                 .match(String.class, this::handleString)
+                .match(RuntimeException.class, this::handleRuntimeException)
                 .matchAny(this::handleUnknownMessages)
                 .build();
     }
 
     private void handleString(String data) {
+        log.info("handleString: {}", data);
+
         String reversed = new StringBuilder(data).reverse().toString();
         sender().tell(reversed, ActorRef.noSender());
     }
 
-    private void handleUnknownMessages(Object data) {
+    private void handleRuntimeException(RuntimeException e) {
+        log.info("handleRuntimeException: {}", e);
+
+        sender().tell(e, ActorRef.noSender());
+    }
+
+    private void handleUnknownMessages(Object data) throws Exception {
+        log.info("handleUnknownMessages: {}", data);
+
+        if (data instanceof Exception) {
+            log.info("handleUnknownMessages: throw {}", data);
+
+            throw (Exception) data;
+        }
+
         sender().tell(new IllegalArgumentException("Unknown type"), ActorRef.noSender());
     }
 }
--- ch2/homework/src/test/java/ua/dp/akka/ReverserActorTest.java
+++ ch2/homework/src/test/java/ua/dp/akka/ReverserActorTest.java
@@ -2,11 +2,14 @@ package ua.dp.akka;
 
 import static akka.pattern.Patterns.ask;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static scala.compat.java8.FutureConverters.toJava;
 
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+import akka.pattern.AskTimeoutException;
 import org.junit.Test;
 
 import akka.actor.ActorSystem;
@@ -16,8 +19,9 @@ import scala.concurrent.Future;
 
 public class ReverserActorTest {
 
-	public static final int TIMEOUT_MILLIS = 100;
-	ActorSystem system = ActorSystem.create();
+	private static final int TIMEOUT_MILLIS = 100;
+
+	private ActorSystem system = ActorSystem.create();
 
 	@Test
 	public void actorShouldReturnReversedString() throws Exception {
@@ -32,31 +36,70 @@ public class ReverserActorTest {
 		//then
 		assertEquals("egassem tseT", actual);
 	}
-//
-//	@Test
-//	public void actorShouldReturnExceptionIfUnknownMessageWasSend() throws Exception {
-//		//given
-//		TestActorRef<ReverserActor> actorRef = TestActorRef.create(system, Props.create(ReverserActor.class));
-//
-//		//when
-//		Future sFuture = ask(actorRef, new StringBuilder("Test message"), TIMEOUT_MILLIS);
-//		CompletableFuture<String> resultFuture = (CompletableFuture<String>)toJava(sFuture);
-////		resultFuture.get(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
-//
-//		//then exception
-//
-//		resultFuture
-//				.handle((x, t) -> {
-//					System.out.println("==> " + x + " === " + t);
-//					if (t != null) {
-//						assertEquals(IllegalArgumentException.class, t.getClass());
-//						assertEquals("Test", t.getMessage());
-//					}
-//					fail("Exception should happend");
-//
-//					return null;
-//				});
-//
-//		Thread.sleep(100);
-//	}
+
+	@Test
+	public void actorShouldReturnExceptionIfUnknownMessageWasSend() throws Exception {
+		//given
+		TestActorRef<ReverserActor> actorRef = TestActorRef.create(system, Props.create(ReverserActor.class));
+
+		//when
+		Future sFuture = ask(actorRef, new StringBuilder("Test message"), TIMEOUT_MILLIS);
+		CompletableFuture<Object> resultFuture = (CompletableFuture<Object>)toJava(sFuture);
+		Object actual = resultFuture.get();
+
+		//then
+		assertEquals(IllegalArgumentException.class, actual.getClass());
+		assertEquals("Unknown type", ((IllegalArgumentException) actual).getMessage());
+	}
+
+	@Test
+	public void timeoutExceptionShouldBeHappenedIfExceptionHappenedInActor() throws Exception {
+		//given
+		TestActorRef<ReverserActor> actorRef = TestActorRef.create(system, Props.create(ReverserActor.class));
+
+		//when
+		Future sFuture = ask(actorRef, new IOException("Created from test"), TIMEOUT_MILLIS);
+		CompletableFuture<Object> resultFuture = (CompletableFuture<Object>)toJava(sFuture);
+		Object actual = resultFuture
+				.handle((x,a) -> null == x ? a : x)
+				.get();
+
+		//then
+		assertEquals(AskTimeoutException.class, actual.getClass());
+		assertEquals(
+				"Ask timed out on [TestActor[akka://default/user/$$d]] after [100 ms]. Sender[null] sent message of type \"java.io.IOException\".",
+				((AskTimeoutException) actual).getMessage());
+	}
+
+
+	@Test
+	public void specialHandlerShouldWorkForRuntimeExceptionInstance() throws Exception {
+		//given
+		TestActorRef<ReverserActor> actorRef = TestActorRef.create(system, Props.create(ReverserActor.class));
+
+		//when
+		Future sFuture = ask(actorRef, new RuntimeException("Created from test"), TIMEOUT_MILLIS);
+		CompletableFuture<Object> resultFuture = (CompletableFuture<Object>)toJava(sFuture);
+		Object actual = resultFuture
+				.get();
+
+		//then
+		assertEquals(RuntimeException.class, actual.getClass());
+		assertEquals("Created from test", ((RuntimeException) actual).getMessage());
+	}
+
+	@Test
+	public void specialHandlerShouldWorkForInstanceThatExtendRuntimeException() throws Exception {
+		//given
+		TestActorRef<ReverserActor> actorRef = TestActorRef.create(system, Props.create(ReverserActor.class));
+
+		//when
+		Future sFuture = ask(actorRef, new NullPointerException("Created from test"), TIMEOUT_MILLIS);
+		CompletableFuture<Object> resultFuture = (CompletableFuture<Object>)toJava(sFuture);
+		Object actual = resultFuture.get();
+
+		//then
+		assertEquals(NullPointerException.class, actual.getClass());
+		assertEquals("Created from test", ((NullPointerException) actual).getMessage());
+	}
 }
